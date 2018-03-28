eb34dd449df4ec2068d36a01fd07b6aee4e40c81
alon.ashkenazi <alon.ashkenazi@600900B211DE408>
2018-03-28 11:54:48 +1100

    Added asteroid spawner

--- Assets/~Asteroids/Scripts/Bullet.cs
+++ Assets/~Asteroids/Scripts/Bullet.cs
@@ -13,10 +13,10 @@ namespace Asteroids
             // If we hit an asteroid
             if (col.name.Contains("Asteroid"))
             {
-
+                GameManager.AddScore(1);
                 // Destroy the asteroid
                 Destroy(col.gameObject);
-                //
+                // Destroy self
                 Destroy(this.gameObject);
             }
         }
--- Assets/~Asteroids/Scripts/GameManager.cs
+++ Assets/~Asteroids/Scripts/GameManager.cs
@@ -2,15 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameManager : MonoBehaviour {
+using UnityEngine.UI;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+namespace Asteroids
+{
+
+
+    public class GameManager : MonoBehaviour
+    {
+        public Text scoreText;
+
+        private static int score = 0;
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            scoreText.text = "Score: " + score.ToString();
+        }
+
+        public static void AddScore(int scoreToAdd)
+        {
+            score += scoreToAdd;
+        }
+    }
 }
