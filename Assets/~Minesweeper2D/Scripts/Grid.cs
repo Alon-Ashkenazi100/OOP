a562f3f0e59ce1a41f3bf28cdabdea54b02ee64a
Ashkenazi <Ashkenazi@GUEST>
2018-04-04 12:01:48 +1000

    Added Minesweeper2D game

---
+++ Assets/~Minesweeper2D/Scripts/Grid.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Minesweeper2D
+{
+    public class Grid : MonoBehaviour
+    {
+        public GameObject tilePrefab;
+        public int width = 10;
+        public int height = 10;
+        public float spacing = .1555f;
+
+        private Tile[,] tiles;
+        // Functionality for spawning tiles
+        Tile SpawnTile(Vector3 pos)
+        {
+            // Clone tile prefab
+            GameObject clone = Instantiate(tilePrefab);
+            clone.transform.position = pos; // position tile
+            Tile currentTile = clone.GetComponent<Tile>(); // Get Tile component
+            return currentTile; //return it
+        }
+
+        // Spawns tiles in a grid-like pattern
+        void GenerateTiles()
+        {
+            // Create new 2D array of size width x height
+            tiles = new Tile[width, height];
+
+            // Loop through the entire tile list
+            for (int x = 0; x < width; x++)
+            {
+                for (int y = 0; y < height; y++)
+                {
+                    // >> Part 2 goes here <<
+                }
+            }
+        }
+    }
+}
+        
---
+++ Assets/~Minesweeper2D/Scripts/Tile.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Minesweeper2D
+{
+    [RequireComponent(typeof(SpriteRenderer))]
+    public class Tile : MonoBehaviour
+    {
+        // Store x and y coordinate in array for later use
+        public int x = 0;
+        public int y = 0;
+        public bool isMine = false; // Is the current tile mine?
+        public bool isRevealed = false; // Has the tile already been revealed?
+        [Header("References")]
+        public Sprite[] emptySprites; // List of empty sprites i.e, empty, 1, 2, 3, 4, etc...
+        public Sprite[] mineSprites; // The mine sprites
+
+        private SpriteRenderer rend; // Reference to sprite renderer
+
+        void Awake()
+        {
+            // Grab reference to sprite renderer
+            rend = GetComponent<SpriteRenderer>();
+        }
+
+        void Start()
+        {
+            // Randomly decide if it's a mine or not
+            isMine = Random.value < .05f;
+        }
+
+        public void Reveal(int adjacentMines, int mineState = 0)
+        {
+            // Flags the tile as being revealed
+            isRevealed = true;
+            // Checks if tile is a mine
+            if (isMine)
+            {
+                // Sets sprite to mine sprite
+                rend.sprite = mineSprites[mineState];
+            }
+            else
+            {
+                // Sets sprite to appropriate texture based on adjacent mines
+                rend.sprite = emptySprites[adjacentMines];
+            }
+        }
+    }
+}
